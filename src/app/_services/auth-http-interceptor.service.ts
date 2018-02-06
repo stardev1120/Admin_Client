12d589535ac2047b6967adfcd0e7daf22161105c
Mohamed Mamdouh <mmamdouh86@gmail.com>
2018-02-07 00:33:01 +0200

    small issue

--- src/app/_services/auth-http-interceptor.service.ts
+++ src/app/_services/auth-http-interceptor.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
     HttpErrorResponse,
     HttpEvent,
@@ -8,23 +8,24 @@ import {
     HttpResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/observable/throw';
-import { Router } from "@angular/router";
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
     constructor(private _router: Router) {
 
     }
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(req)
             .map((event: HttpEvent<any>) => {
                 /*if (event instanceof HttpResponse && ~(event.status / 100) > 3) {
-                    console.info('HttpResponse::event =', event, ';');
-                } else console.info('event =', event, ';');*/
+                 console.info('HttpResponse::event =', event, ';');
+                 } else console.info('event =', event, ';');*/
                 return event;
             })
             .catch((err: any, caught) => {
@@ -32,7 +33,8 @@ export class AuthInterceptor implements HttpInterceptor {
                     if (err.status === 403) {
                         console.info('err.error =', err.error, ';');
                     }
-                    if ((err.status === 401 && (err.error !== 'You didn\'t a super admin or didn\'t have valid OTP') || err.error !== 'you are not a admin') || err.error === 'jwt expired') {
+                    //(err.status === 401 && (err.error !== 'You didn\'t a super admin or didn\'t have valid OTP') || err.error !== 'you are not a admin') ||
+                    if (err.error === 'jwt expired') {
                         console.log(err);
                         location.reload()
                     }
